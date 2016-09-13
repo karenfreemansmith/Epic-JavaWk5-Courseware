2194d0085ec1e704646820a9b823fcfd684e1bc4
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-13 05:47:33 -0700

    update template, minor fixes and README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Project Title
+Version 0.0.0: September 13, 2016
+
+by [Karen Freeman-Smith](https://github.com/karenfreemansmith)
+
+## Description
+A program to ...
+
+### Specifications
+
+## Setup/Installation
+* Clone directory
+* Type 'gradle run' inside the directory
+* Navigate to 'http://localhost:5467'
+
+## Support & Contact
+For questions, concerns, or suggestions please email karenfreemansmith@gmail.com
+
+## Known Issues
+* N/A
+
+## Technologies Used
+Java, JUnit, Spark, Gradle
+
+## Legal
+*Licensed under the GNU General Public License v3.0*
+
+Copyright (c) 2016 Copyright _Karen Freeman-Smith_ All Rights Reserved.
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
+import java.util.Map;
 import java.util.HashMap;
+
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -7,6 +9,19 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
-    
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/result", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("result", request.queryParams("item1"));
+      model.put("template", "templates/result.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
