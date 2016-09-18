c12ecfe571ba3e5a057eba6610277ff21fe24b35
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-17 20:51:23 -0700

    add database to boilerplate

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,19 @@
 public class Task {
-  
+
+  // public static List<Task> all() {
+  //   String sql = "SELECT id, description FROM tasks";
+  //   try(Connection con = DB.sql2o.open()) {
+  //     return con.createQuery(sql).executeAndFetch(Task.class);
+  //   }
+  // }
+
+  // @Override
+  // public boolean equals(Object otherTask) {
+  //   if (!(otherTask instanceof Task)) {
+  //     return false;
+  //   } else {
+  //     Task newTask = (Task) otherTask;
+  //     return this.getDescription().equals(newTask.getDescription());
+  //   }
+  // }
 }
