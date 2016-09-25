5e4b31c61f20221cd6fdcad34e1c31bfeecb0206
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-24 17:20:47 -0700

    add heroku code to template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
 
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -10,13 +11,23 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+    ProcessBuilder process = new ProcessBuilder();
+    Integer port;
+    if (process.environment().get("PORT") != null) {
+       port = Integer.parseInt(process.environment().get("PORT"));
+    } else {
+       port = 4567;
+    }
+
+    setPort(port);
+
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/result", (request, response) -> {
+    post("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("result", request.queryParams("item1"));
       model.put("template", "templates/result.vtl");
