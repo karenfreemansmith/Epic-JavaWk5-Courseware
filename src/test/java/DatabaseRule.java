d9bd591054c6fe2cee2208099caca2b615677e1a
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-10-06 17:13:02 -0700

    change database connections to home settings

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,7 +5,7 @@ public class DatabaseRule extends ExternalResource {
 
   @Override
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/courseware_test", null, null);
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/courseware_test", "postgres", null);
   }
 
   @Override
